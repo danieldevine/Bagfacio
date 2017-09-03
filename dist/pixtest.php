f1d3142118126a48ef1ac1d1767d26659c0d7948
Dan Devine <dan@maverick-intl.com>
2017-09-04 00:17:22 +0100

    prepare

--- dist/pixtest.php
+++ dist/pixtest.php
@@ -1,14 +1,13 @@
 <?php
 
 $root     = $_SERVER["DOCUMENT_ROOT"];
-$site_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 
 include_once $root . '/inc/components/loaders/loader.php';
 
 
 
 
-$pixelImage = new LoremPixel( 600, 600, $site_url );
+$pixelImage = new LoremPixel( 600, 600, $root );
 $thisImage = $pixelImage->getLoremPixel();
 
 echo $thisImage;
