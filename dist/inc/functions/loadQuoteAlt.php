fa6328dd89f308b13be4598e51f6e52cd154210f
Dan Devine <dan@maverick-intl.com>
2017-09-05 10:06:04 +0100

    resizes text

--- dist/inc/functions/loadQuoteAlt.php
+++ dist/inc/functions/loadQuoteAlt.php
@@ -5,35 +5,29 @@ function loadQuoteAlt($imgname, $quote)
     /* Attempt to open */
     $im = @imagecreatefromjpeg($imgname);
     $tc  = imagecolorallocate($im, 0, 0, 0);
-
     imagestring($im, 1, 5, 5, $quote, $tc);
 
-    // Create some colors
-    // 
-    imagealphablending($im, true); 
+    imagealphablending($im, true);
 
     $white = imagecolorallocate($im, 255, 255, 255);
     $grey = imagecolorallocate($im, 128, 128, 128);
     $black = imagecolorallocate($im, 0, 0, 0);
     $bag = imagecolorallocate($im, 74, 179, 244);
-    $transparent = imagecolorallocatealpha($im, 74, 179, 244, 45); 
+    $transparent = imagecolorallocatealpha($im, 74, 179, 244, 45);
+
     imagefilledrectangle($im, 0, 0, 600, 30, $bag);
-
     imagefilledrectangle($im, 0, 600, 600, 550, $bag);
-
     imagefilledrectangle($im, 0, 600, 600, 0, $transparent);
 
-
-    // Replace path by your own font path
     $font = $_SERVER["DOCUMENT_ROOT"] .'/assets/fonts/pacifico.ttf';
     $funt = $_SERVER["DOCUMENT_ROOT"] .'/assets/fonts/jollylodger.ttf';
 
     // Add some shadow to the text
-    imagettftext($im, 60, 0, 100, 224, $black, $funt, $quote);
+    imagettftext($im, 40, 0, 48, 150, $black, $funt, $quote);
 
     // Add the text
     // image, font size, rotation, left, top
-    imagettftext($im, 60, 0, 100, 220, $white, $funt, $quote);
+    imagettftext($im, 40, 0, 50, 160, $white, $funt, $quote);
 
     // tag the fecker
     imagettftext($im, 12, 0, 230, 580, $black, $font, 'Bagfacio Best Friendos');
