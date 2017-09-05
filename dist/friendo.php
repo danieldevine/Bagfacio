c33826e161f690550a9d49df619d0024a747e77d
Dan Devine <dan@maverick-intl.com>
2017-09-05 09:31:17 +0100

    spacing

--- dist/friendo.php
+++ dist/friendo.php
@@ -1,43 +1,36 @@
 <?php
-
-$root     = $_SERVER["DOCUMENT_ROOT"];
-$site_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-
+$root       = $_SERVER["DOCUMENT_ROOT"];
+$site_url   = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 include_once $root . '/inc/components/loaders/loader.php';
 require_once("$root/vendor/abraham/twitteroauth/autoload.php");
-
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$name  = 'flickr';
-$theme = 'fun';
-$image = new LoremFlickr(600, 600, $name, $theme);
-$image = $image->getLoremFlickr();
-
+$name       = 'flickr';
+$theme      = 'fun';
+$image      = new LoremFlickr(600, 600, $name, $theme);
+$image      = $image->getLoremFlickr();
 
 $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+$followers  = new Followers($connection);
+$followers  = $followers->getFollowers();
 
-$followers = new Followers($connection);
-$followers = $followers->getFollowers();
+include 'inc/data/colors.php';
+
+$faveColour = $colours[0];
+
 
 header('Content-Type: image/jpeg');
-$friendo =  " @" .$followers[0]. " + \n" . "@".$followers[1];
-$img = loadQuoteAlt($image, $friendo);
+$friendo    = " @" .$followers[0]. " + \n" . "@".$followers[1];
+$img        = loadQuoteAlt($image, $friendo);
 imagepng($img, 'flarp.png' );
 imagedestroy($img);
-$image = $site_url.'/flarp.png';
+$image      = $site_url.'/flarp.png';
 
+$message    = "Hi there @".$followers[0]." : Your twitter wife is  @" . $followers[1] . ". You should follow @" . $followers[2] . ", @" . $followers[3] .  " and @coderjerk";
 
-$message =  "Hi there @".$followers[0]." : Your twitter wife is  @" . $followers[1] . ". You should follow @" . $followers[2] . ", @" . $followers[3] .  " and @coderjerk";
-
-/**
- * Upload the status and image prior to posting.
- */
-$media = $connection->upload('media/upload', ['media' => $image]);
+$media      = $connection->upload('media/upload', ['media' => $image]);
 $parameters = [
     'status' => $message,
     'media_ids' => implode(',', [$media->media_id_string])
 ];
-/**
- * post to Twitter
- */
-$result = $connection->post('statuses/update', $parameters);
+$result     = $connection->post('statuses/update', $parameters);
--- dist/inc/components/loaders/loader.php
+++ dist/inc/components/loaders/loader.php
@@ -1,16 +1,18 @@
 <?php
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+
 require_once("$root/config/fb.php");
 require_once("$root/config/twitter.php");
+
 require_once("$root/vendor/autoload.php");
+
 require_once("$root/inc/classes/dataMuse.php");
 require_once("$root/inc/classes/LoremPixel.php");
 require_once("$root/inc/classes/LoremFlickr.php");
 require_once("$root/inc/classes/Followers.php");
+
 require_once("$root/inc/functions/loadQuoteJpg.php");
 require_once("$root/inc/functions/loadQuoteAlt.php");
-
-
 
 //build the auth for Facebook
 $fb = new Facebook\Facebook([
@@ -18,4 +20,3 @@ $fb = new Facebook\Facebook([
  'app_secret'            => $appSecret,
  'default_graph_version' => 'v2.8',
 ]);
-?>
